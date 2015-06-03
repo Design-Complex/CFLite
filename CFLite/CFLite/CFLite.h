c75571a412f61c0ec1bc8658158c5f7205a75b6c
Rob Dotson <robdotson@lappytop.local>
2015-06-03 09:47:02 -0400

    Created Xcode project file

---
+++ CFLite/CFLite/CFLite.h
@@ -0,0 +1,19 @@
+//
+//  CFLite.h
+//  CFLite
+//
+//  Created by Rob Dotson  on 6/3/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CFLite.
+FOUNDATION_EXPORT double CFLiteVersionNumber;
+
+//! Project version string for CFLite.
+FOUNDATION_EXPORT const unsigned char CFLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CFLite/PublicHeader.h>
+
+
